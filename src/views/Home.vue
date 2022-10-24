dc52ec9a77d99181c73b0ada50cdb65f976c2a3d
Eric-Wang159 <73349489+Eric-Wang159@users.noreply.github.com>
2022-10-24 18:02:49 +0200

    final projet final CSS

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,8 @@
 <template>
 
-  <div style=" top: 50%; right: 60%; display: flex; flex-wrap: wrap; justify-content: center;" class="home">
+  <div style=" top: 50%; right: 60%; display: flex; flex-wrap: wrap; justify-content: center; " class="home">
     <img style=" top: 40%; right:25%; width: 300px; padding:20px;" src="../assets/earth_chanq.jpg"/>
-    <img style="width: 300px; padding: 20px;" src="../assets/aquaq.gif"/>
+    <img style=" height:300px; padding: 20px; align-self: center;" src="../assets/aquaq.gif"/>
   </div>
   <p style="font-size: x-large; font-weight: bold;"> Planet is dying :( </p>
 </template>
